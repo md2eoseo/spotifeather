a998aaa7bf21c0483ea33611834110f7ffecbb4c
md2eoseo <md2eoseo@gmail.com>
2021-07-01 17:38:31 +0900

    remove duplicated files

--- client/src/playlistsActions.js
+++
@@ -1,6 +0,0 @@
-export const SET_PLAYLISTS = 'playlists/SET_PLAYLISTS';
-
-export const setPlaylists = playlists => ({
-  type: SET_PLAYLISTS,
-  payload: playlists,
-});
--- client/src/weatherActions.js
+++
@@ -1,6 +0,0 @@
-export const SET_WEATHER = 'weather/SET_WEATHER';
-
-export const setWeather = weather => ({
-  type: SET_WEATHER,
-  payload: weather,
-});
