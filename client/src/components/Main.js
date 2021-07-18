99f2cc58f2ed284da52f99c6fca56ef0f7cbe1e1
md2eoseo <md2eoseo@gmail.com>
2021-07-18 18:08:14 +0900

    random search term for playlists

--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -16,6 +16,25 @@ function Main() {
   const setAccessTokenWithCurrentUser = token => dispatch(setAccessToken(token));
   const setPlaylistsWithRecent = playlists => dispatch(setPlaylists(playlists));
 
+  // 날씨에 맞는 플레이리스트 검색어 랜덤 선택
+  const chooseCategoryTerm = () => {
+    const prefix = weather.id % 800 === 0 ? 0 : weather.id % 100;
+    const category = {
+      0: ['clear sky', 'sunny day', 'blue sky day'],
+      2: ['thunder', 'thunderstorm', 'lightning night', 'thunder night'],
+      3: ['drizzle', 'drizzle night', 'drizzle morning'],
+      5: ['raining', 'raining night', 'raining cafe', 'raining morning'],
+      6: ['snowy', 'snowing night', 'snowing cafe', 'snowflake'],
+      7: ['foggy', 'foggy morning'],
+      8: ['cloudy morning', 'cloudy night', 'cloudy'],
+    };
+    const randomNum = getRandomNum(category[prefix].length);
+    return category[prefix][randomNum];
+  };
+  const getRandomNum = max => {
+    return Math.floor(Math.random() * max);
+  };
+
   useEffect(() => {
     if (accessToken) setAccessTokenWithCurrentUser(accessToken);
   }, []);
@@ -23,9 +42,11 @@ function Main() {
   // 플레이리스트 불러오기
   useEffect(() => {
     if (user.token) {
+      const term = chooseCategoryTerm(weather);
+      console.log(term);
       var headers = new Headers();
       headers.append('Authorization', `Bearer ${user.token}`);
-      fetch(`https://api.spotify.com/v1/search?q=${weather.main}&type=playlist&limit=4`, { headers })
+      fetch(`https://api.spotify.com/v1/search?q=${term}&type=playlist&limit=4`, { headers })
         .then(res => res.json())
         .then(data => setPlaylistsWithRecent(data?.playlists?.items));
       console.log('플레이리스트 불러오기');
