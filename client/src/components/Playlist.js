34a133f7a052d1719850f2b62cfb2b8689b44ea9
md2eoseo <md2eoseo@gmail.com>
2021-07-18 12:32:39 +0900

    simple slider for playlists

--- client/src/components/Playlist.js
+++ client/src/components/Playlist.js
@@ -1,4 +1,12 @@
-function Playlist({ id }) {
-  return <iframe className="playlist" src={`https://open.spotify.com/embed/playlist/${id}`} allow="encrypted-media"></iframe>;
+function Playlist({ id, show }) {
+  return (
+    <iframe
+      title={id}
+      className="playlist"
+      src={`https://open.spotify.com/embed/playlist/${id}`}
+      allow="encrypted-media"
+      style={{ display: show ? 'block' : 'none' }}
+    ></iframe>
+  );
 }
 export default Playlist;
--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -1,13 +1,31 @@
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Playlist from './Playlist';
 
 function Playlists() {
   const playlists = useSelector(state => state.playlists);
+  const [page, setPage] = useState(playlists.length);
   return (
     <div className="playlists">
-      {playlists?.map(pl => (
-        <Playlist key={pl.id} id={pl.id} />
+      <button
+        className="pageBtn"
+        onClick={() => {
+          setPage((page - 1 + playlists.length) % playlists.length);
+        }}
+      >
+        <span>{'<'}</span>
+      </button>
+      {playlists?.map((pl, idx) => (
+        <Playlist key={pl.id} id={pl.id} show={idx === page} />
       ))}
+      <button
+        className="pageBtn"
+        onClick={() => {
+          setPage((page + 1) % playlists.length);
+        }}
+      >
+        <span>{'>'}</span>
+      </button>
     </div>
   );
 }
