834fd726c4fa891c37b4f126174baa4b52b1df3d
md2eoseo <md2eoseo@gmail.com>
2021-07-21 19:54:37 +0900

    added react-spotify-web-playback

--- client/src/components/Playlists.js
+++ client/src/components/Playlists.js
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Playlist from './Playlist';
 import ReactLoading from 'react-loading';
+import SpotifyWebPlayer from 'react-spotify-web-playback';
 
 function Playlists() {
+  const user = useSelector(state => state.user);
   const playlists = useSelector(state => state.playlists);
   const [page, setPage] = useState(playlists.length);
   return playlists.length !== 0 ? (
@@ -19,6 +21,7 @@ function Playlists() {
       {playlists?.map((pl, idx) => (
         <Playlist key={pl.id} id={pl.id} show={idx === page} />
       ))}
+
       <button
         className="pageBtn"
         onClick={() => {
@@ -27,6 +30,23 @@ function Playlists() {
       >
         <span>{'>'}</span>
       </button>
+      <SpotifyWebPlayer
+        token={user.accessToken}
+        uris={playlists.map(pl => pl.uri)[page]}
+        styles={{
+          activeColor: '#fff',
+          bgColor: '#333',
+          color: '#fff',
+          loaderColor: '#fff',
+          sliderColor: '#1cb954',
+          trackArtistColor: '#ccc',
+          trackNameColor: '#fff',
+        }}
+        autoPlay
+        magnifySliderOnHover
+        name="Spotifeather"
+        showSaveIcon
+      />
     </div>
   ) : (
     <ReactLoading type="bars" color="#fff" width="120px" height="60px" />
